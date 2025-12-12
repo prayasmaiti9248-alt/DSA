19afa5dfd14d1e956b33a6660e2564e186d3d073
prayasmaiti9248-alt <prayasmaiti9248@gmail.com>
2025-12-12 19:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.44%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            vector<int> ans;
-            int temp=n;
-            while(temp>0){
-                ans.push_back(temp%i);
-                temp=temp/i;
-            }
-            int l=0;
-            int r=ans.size()-1;
-            while(l<r){
-                if(ans[l]!=ans[r])
-                return false;
-                l++;
-                r--;
-            }
-        }
-        return true;
+        return false;
     }
 };
