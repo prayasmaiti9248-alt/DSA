6aa6563e1ca3b55445e6aa1ca046e9a34f50ec8d
prayasmaiti9248-alt <prayasmaiti9248@gmail.com>
2025-12-31 16:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 80.3 MB (89.97%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    vector<int> findArray(vector<int>& pref) {
-       int p=pref[0];
-       vector<int> ans;
-       ans.push_back(p);
-       for(int i=1;i<pref.size();i++){
-        p=pref[i]^pref[i-1];
-        ans.push_back(p);
-       } 
-       return ans;
+    vector<int> findArray(vector<int>& pref){
+        int n = pref.size();
+        vector<int> arr(n);
+        
+        arr[0] = pref[0];
+        
+        for (int i = 1; i < n; i++) {
+            // The current element reverses the change made from the previous prefix
+            arr[i] = pref[i-1] ^ pref[i];
+        }
+        
+        return arr;
     }
 };
