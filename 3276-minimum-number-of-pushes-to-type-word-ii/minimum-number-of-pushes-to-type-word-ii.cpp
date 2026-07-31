0744c399f77f1b9f20e4edf169ae5720bab5ea1f
prayasmaiti9248-alt <prayasmaiti9248@gmail.com>
2026-08-01 00:48:16 +0530

    Time: 1 ms (96.43%) | Memory: 25.4 MB (84.45%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+       vector<int> freq(26,0);
+       for(int i=0;i<word.size();i++){
+        freq[word[i]-'a']++;
+       } 
+       sort(freq.begin(),freq.end());
+       int ans=0;
+       for(int i=25;i>=0&&freq[i]>0;i--){
+        ans+=freq[i]*((25-i)/8 + 1);
+       }
+       return ans;
+    }
+};
